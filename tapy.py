0bcf8f8ebd0f52f10d7b8d3998bc03e3c80d8c40
AverseABFun <81586710+AverseABFun-windows@users.noreply.github.com>
2023-02-19 04:05:08 +0000

    changed main.py to test.py, added gitignore, almost fully implemented World class, created Command class, modified Item class to be more then a normal Object, changed .replit.

--- tapy.py
+++ tapy.py
@@ -1,32 +1,68 @@
 class Room:
-    def __init__(self, name, desc, id, exits, items=[]):
-      self.name = name
-      self.desc = desc
-      self.id = id
-      self.exits = exits
-      self.items = items
-    def __repr__(self):
-      return f'TA.Room(name={self.name}, desc={self.desc}, id={self.id}, exits={self.exits})'
-  
+
+  def __init__(self, name, desc, id, exits, items=[]):
+    self.name = name
+    self.desc = desc
+    self.id = id
+    self.exits = exits
+    self.items = items
+
+  def __repr__(self):
+    return f'Room(name={self.name}, desc={self.desc}, id={self.id}, exits={self.exits})'
+
+
 class Object:
+
   def __init__(self, name: str, sdesc: str, ldesc: str, id: str, location):
     self.name = name
     self.sdesc = sdesc
     self.ldesc = ldesc
     self.id = id
     self.loc = location
+
   @property
-  def location(self): return self.loc
+  def location(self):
+    return self.loc
+
   def move(self, newloc):
     self.loc = newloc
-class Item(Object): pass
+
+
+class Item(Object):
+
+  def move(self, newloc):
+    super(Item, self).move(newloc)
+    newloc.items.append(self)
+
+
 class Player(Object):
+
   def __init__(self, startloc):
     super(Player, self).__init__("player", "", "", "p1p", startloc)
-    self.inventory = Room("Inventory", "How did you get here?", "p1i",[])
+    self.inventory = Room("Inventory", "How did you get here?", "p1i", [])
+
+  def pickup(self, item: Item):
+    item.move(self.inventory)
+
+
+class Command:
+
+  def __init__(self, name, func, desc):
+    self.name = name
+    self.func = func
+    self.desc = desc
 
 
 class World:
-    def __init__(self, start, cmds):
-      self.start = start
-      self.cmds = cmds
+
+  def __init__(self, start, cmds):
+    self.start = start
+    self.cmds = cmds
+    self.player = Player(start)
+
+  def run(self, prompt="> "):
+    while True:
+      inp = input(self.player.loc.name + prompt)
+      for cmd in self.cmds:
+        if inp.startswith(cmd.name):
+          cmd.func(inp)
